81e5d9390ca5816f8e211885015f0ac4109c9b60
karen-ifrt <55798470+karen-ifrt@users.noreply.github.com>
2019-11-08 16:54:17 +0100

    Vue.js

---
+++ Vue js/app.js
@@ -0,0 +1,26 @@
+new Vue({
+    el: '#app',
+
+    data: {
+
+        message: 'Hello Vue.js !',
+
+        // link: 'https://fr.vuejs.org',
+
+     success: false,
+
+         ok: false,
+
+        // frameworks: ['Angular', 'React', 'Vue']
+    },
+
+    methods: {
+        // close: function () {
+        //     this.success = false
+        // }
+    }
+
+
+
+
+})
